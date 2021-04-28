94ca8e020e7a6c9e1370b71dcc7e1bd19c8999dd
etigerstudio <etigerstudio@gmail.com>
2021-04-28 21:23:57 +0800

    impl: bi-gram baseline result.

--- bgram.py
+++ bgram.py
@@ -2,156 +2,133 @@
 # Bi-gram 二元语法分词
 # ========================================
 
-import utils
 from utils import BOS, EOS
 from math import log
 from sys import maxsize
 
-"""
-总体步骤:
-    1. 统计一元词频
-    2. 统计二元词频
-    3. 切词生成词网
-    4. 计算最大概率路径
-"""
 
-# 1. 统计一元词频
-# 2. 统计二元词频
-# uni_freq, bi_freq = utils.read_bigram_words("short.txt")
-uni_freq, bi_freq = utils.read_bigram_words("training.txt")
-vocabulary = set(uni_freq.keys())
-max_word_len = max([len(w) for w in vocabulary])
-total_word_count = len(uni_freq)
-total_word_frequency = sum(uni_freq.values())
+class BiGram:
+    def __init__(self, uni_freq, bi_freq):
+        self.uni_freq = uni_freq
+        self.bi_freq = bi_freq
+        self.vocabulary = set(uni_freq.keys())
+        self.max_word_len = max([len(w) for w in self.vocabulary])
+        self.total_word_count = len(uni_freq)
+        self.total_word_frequency = sum(uni_freq.values())
 
-SMOOTHING_LAMBDA = 0.1
-SMOOTHING_MU = 1 / total_word_frequency + 0.00001
-# SMOOTHING_MU = 0.00000001
+        self.SMOOTHING_LAMBDA = 0.1
+        self.SMOOTHING_MU = 1 / self.total_word_frequency + 0.00001
 
-# 3. 切词生成词网
-# test_sen = "共同创造美好的新世纪"
-# test_sen = "一起共同创造美好的二十一世纪"
-# test_sen = "共同创造美好的新世纪——二○○一年新年贺词"
-# test_sen = "（二○○○年十二月三十一日）（附图片1张）"
-test_sen = "在这个激动人心的时刻，我很高兴通过中国国际广播电台、中央人民广播电台和中央电视台，向全国各族人民，向香港特别行政区同胞、澳门特别行政区同胞和台湾同胞、海外侨胞，向世界各国的朋友们，致以新世纪第一个新年的祝贺！"
-test_sen_len = len(test_sen)
-word_net = []
-for i in range(0, len(test_sen)):
-    word_net.append([])
-    # for j in range(1, min(max_word_len, test_sen_len - i) + 1):
-    #     if test_sen[i:i + j] in vocabulary:
-    #         word_net[i].append(test_sen[i:i + j])
-    # # if len(word_net[i]) == 0:  # 避免断路
-    # #     word_net[i].append(test_sen[i])  # TODO: 精确判断是否断路
-    word_net[i].append(test_sen[i])  # 默认切分单字词
-    for j in range(2, min(max_word_len, test_sen_len - i) + 1):
-        if test_sen[i:i + j] in vocabulary:
-            word_net[i].append(test_sen[i:i + j])
-word_net.insert(0, [BOS])
-word_net.append([EOS])
-print(word_net)
+    def coarse_word_net_segment(self, sentence, vocabulary):
+        sen_len = len(sentence)
+        word_net = []
+        for i in range(0, len(sentence)):
+            word_net.append([])
+            word_net[i].append(sentence[i])  # 默认切分单字词
+            for j in range(2, min(self.max_word_len, sen_len - i) + 1):
+                if sentence[i:i + j] in vocabulary:
+                    word_net[i].append(sentence[i:i + j])
+        word_net.insert(0, [BOS])
+        word_net.append([EOS])
+        return word_net
 
+    def calc_plain_probability(self, w1, w2):
+        if self.bi_freq[w1][w2] == 0:
+            return -log(0.0001)  # 将0替换为较小概率
 
-# 4. 计算最大概率路径
-def calc_plain_probability(w1, w2, uni_freq, bi_freq):
-    if bi_freq[w1][w2] == 0:
-        return -log(0.0001)
+        p = -log(self.bi_freq[w1][w2] / self.uni_freq[w1])
+        # print(w1, w2, p)
+        return p
 
-    p = -log(bi_freq[w1][w2] / uni_freq[w1])
-    # print(w1, w2, p)
-    return p
+    def calc_smoothed_probability(self, w1, w2):
+        """
+        P_smoothed(w2|w1) = (1 - l) * ((1 - m) * c(w1w2) / c(w1) + m) + l * ((c(w2) + 1) / N + |V|)
+        P经验平滑公式；P的总和等于1
+        """
+        return \
+            -log((1 - self.SMOOTHING_LAMBDA) * ((1 - self.SMOOTHING_MU) * self.bi_freq[w1][w2] / (self.uni_freq[w1] + self.SMOOTHING_MU)) \
+                 + self.SMOOTHING_LAMBDA * ((self.uni_freq[w2] + 1) / (self.total_word_frequency + self.total_word_count)))
 
+    @staticmethod
+    def dijkstra_shortest_path(graph):
+        """
+        通过Dijkstra算法求解最短路径
 
-def dijkstra_shortest_path(graph):
-    """
-    通过Dijkstra算法求解最短路径
+        :param graph: 有向图权重矩阵
+        :return: 最短路径分割点下标
+        """
+        length = len(graph[0])
+        dist = [maxsize] * length
+        dist[0] = 0
 
-    :param graph: 有向图权重矩阵
-    :return: 最短路径分割点下标
-    """
-    length = len(graph[0])
-    dist = [maxsize] * length
-    dist[0] = 0
+        visited = set()
+        prev = [-1] * length
 
-    visited = set()
-    prev = [-1] * length
+        while len(visited) < length:
+            # 寻找下一个节点
+            min = maxsize
+            for u in set(range(length)) - visited:
+                if dist[u] < min:
+                    min = dist[u]
+                    min_index = u
 
-    while len(visited) < length:
-        # 寻找下一个节点
-        min = maxsize
-        for u in set(range(length)) - visited:
-            if dist[u] < min:
-                min = dist[u]
-                min_index = u
+            # 判断是否已经到达终点
+            if min_index == length - 1:
+                # print("到达终点")
+                break
 
-        # 判断是否已经到达终点
-        if min_index == length - 1:
-            print("到达终点")
-            break
+            # 松弛
+            u = min_index
+            for v in set(range(length)) - visited:
+                if dist[v] > dist[u] + graph[u][v]:
+                    # print(f"发现新极短路径 u:{u} v:{v} 原距离:{dist[v]} 新距离:{dist[u] + graph[u][v]}")
+                    dist[v] = dist[u] + graph[u][v]
+                    prev[v] = u
 
-        # 松弛
-        u = min_index
-        for v in set(range(length)) - visited:
-            if dist[v] > dist[u] + graph[u][v]:
-                print(f"发现新极短路径 u:{u} v:{v} 原距离:{dist[v]} 新距离:{dist[u] + graph[u][v]}")
-                dist[v] = dist[u] + graph[u][v]
-                prev[v] = u
+            visited.add(u)
 
-        visited.add(u)
+        # 回溯路径
+        path = [length - 1]
+        u = length - 1
+        while prev[u] != -1:
+            path.append(prev[u])
+            u = prev[u]
+        path.reverse()
+        # print("最终求解路径：", path)
 
-    # 回溯路径
-    path = [length - 1]
-    u = length - 1
-    while prev[u] != -1:
-        path.append(prev[u])
-        u = prev[u]
-    path.reverse()
-    print("最终求解路径：", path)
+        return path
 
-    return path
+    def construct_graph(self, word_net):
+        length = len(word_net)
+        graph = [[maxsize for _ in range(length)] for _ in range(length)]
 
+        for i in range(1, length):
+            for w2 in word_net[i]:
+                for j in range(1, min(self.max_word_len, i) + 1):
+                    for w1 in word_net[i - j]:
+                        if (len(w1) == j and w1 != BOS) or (j == 1 and w1 == BOS):
+                            # graph[i - j][i] = calc_plain_probability(w1, w2, uni_freq, bi_freq)
+                            graph[i - j][i] = self.calc_smoothed_probability(w1, w2)
+                            # print(f"写入可能分割 w1:{w1} {i - j} w2:{w2} {i} 概率负对数:{graph[i - j][i]}")
 
-def construct_graph(word_net, uni_freq, bi_freq):
-    length = len(word_net)
-    graph = [[maxsize for _ in range(length)] for _ in range(length)]
+        return graph
 
-    for i in range(1, length):
-        for w2 in word_net[i]:
-            for j in range(1, min(max_word_len, i) + 1):
-                for w1 in word_net[i - j]:
-                    if (len(w1) == j and w1 != BOS) or (j == 1 and w1 == BOS):
-                        # graph[i - j][i] = calc_plain_probability(w1, w2, uni_freq, bi_freq)
-                        graph[i - j][i] = calc_smoothed_probability(w1, w2, uni_freq, bi_freq)
-                        print(f"写入可能分割 w1:{w1} {i - j} w2:{w2} {i} 概率负对数:{graph[i - j][i]}")
+    def convert_path_to_segmentation(self, sentence, path):
+        seg = []
 
-    return graph
+        for i in range(2, len(path)):
+            seg.append(sentence[path[i - 1] - 1:path[i] - 1])
 
+        return seg
 
-def convert_path_to_segmentation(sentence, path):
-    seg = []
+    def segment_sentences(self, sentences):
+        results = []
 
-    for i in range(2, len(path)):
-        seg.append(sentence[path[i - 1] - 1:path[i] - 1])
+        for sen in sentences:
+            word_net = self.coarse_word_net_segment(sen, self.vocabulary)
+            graph = self.construct_graph(word_net)
+            path = self.dijkstra_shortest_path(graph)
+            seg = self.convert_path_to_segmentation(sen, path)
+            results.append(seg)
 
-    return seg
-
-
-def calc_smoothed_probability(w1, w2, uni_freq, bi_freq):
-    """
-    P_smoothed(w2|w1) = (1 - l) * ((1 - m) * c(w1w2) / c(w1) + m) + l * ((c(w2) + 1) / N + |V|)
-    P经验平滑公式；P的总和等于1
-    """
-    return \
-        -log((1 - SMOOTHING_LAMBDA) * ((1 - SMOOTHING_MU) * bi_freq[w1][w2] / (uni_freq[w1] + SMOOTHING_MU)) \
-             + SMOOTHING_LAMBDA * ((uni_freq[w2] + 1) / (total_word_frequency + total_word_count)))
-
-
-# w1, w2 = "共同", "创造"
-# w1, w2 = "，", "先生"
-# p = calc_segmentation_probability(w1, w2, uni_freq, bi_freq)
-# print(p)
-graph = construct_graph(word_net, uni_freq, bi_freq)
-# print(graph)
-path = dijkstra_shortest_path(graph)
-seg = convert_path_to_segmentation(test_sen, path)
-# print(seg)
+        return results
--- test_runner.py
+++ test_runner.py
@@ -1,80 +1,98 @@
 import utils
 import mm
 import hmm
-
+import bgram
 
 if __name__ == '__main__':
- unit = "hmm-export"
+    unit = "bigram-export"
 
- if unit == "mm-short":
-  v = utils.read_vocabulary_dataset("training_vocab.txt")
-  s = utils.read_plain_sentences("short.txt")
+    if unit == "mm-short":
+        v = utils.read_vocabulary_dataset("training_vocab.txt")
+        s = utils.read_plain_sentences("short.txt")
 
-  results = mm.tokenize(v, s, mode=mm.MMMode.Forward)
-  print("F", results)
+        results = mm.tokenize(v, s, mode=mm.MMMode.Forward)
+        print("F", results)
 
-  results = mm.tokenize(v, s, mode=mm.MMMode.Reverse)
-  print("R", results)
+        results = mm.tokenize(v, s, mode=mm.MMMode.Reverse)
+        print("R", results)
 
-  results = mm.tokenize(v, s, mode=mm.MMMode.Bidirectional)
-  print("Bi", results)
+        results = mm.tokenize(v, s, mode=mm.MMMode.Bidirectional)
+        print("Bi", results)
 
- elif unit == "mm-export":
-  v = utils.read_vocabulary_dataset("training_vocab.txt")
-  s = utils.read_plain_sentences("test.txt")
+    elif unit == "mm-export":
+        v = utils.read_vocabulary_dataset("training_vocab.txt")
+        s = utils.read_plain_sentences("test.txt")
 
-  results = mm.tokenize(v, s, mode=mm.MMMode.Forward)
-  utils.export_plain_sentences(results, "fmm_result.txt")
+        results = mm.tokenize(v, s, mode=mm.MMMode.Forward)
+        utils.export_plain_sentences(results, "fmm_result.txt")
 
-  results = mm.tokenize(v, s, mode=mm.MMMode.Reverse)
-  utils.export_plain_sentences(results, "rmm_result.txt")
+        results = mm.tokenize(v, s, mode=mm.MMMode.Reverse)
+        utils.export_plain_sentences(results, "rmm_result.txt")
 
-  results = mm.tokenize(v, s, mode=mm.MMMode.Bidirectional)
-  utils.export_plain_sentences(results, "bimm_result.txt")
-
-
- elif unit == "mm-changjiang":
-  v = ["武汉", "市长", "长江大桥"]
-  s = ["武汉市长江大桥"]
-
-  results = mm.tokenize(v, s, mode=mm.MMMode.Forward)
-  print("F", results)
-
-  results = mm.tokenize(v, s, mode=mm.MMMode.Reverse)
-  print("R", results)
-
-  results = mm.tokenize(v, s, mode=mm.MMMode.Bidirectional)
-  print("Bi", results)
+        results = mm.tokenize(v, s, mode=mm.MMMode.Bidirectional)
+        utils.export_plain_sentences(results, "bimm_result.txt")
 
 
- elif unit == "hmm-dummy":
-  states = [[0, 2], [0, 2], [0, 1, 1, 2], [3]]
-  observations = [[53966, 23], [5966, 43], [43, 42, 41, 40], [99]]
+    elif unit == "mm-changjiang":
+        v = ["武汉", "市长", "长江大桥"]
+        s = ["武汉市长江大桥"]
 
-  model = hmm.HMM()
-  model.train(states, observations)
-  print(model.predict(observations))
-  print(model.predict([[53966, 23, 5966, 43, 42, 41, 40]]))
+        results = mm.tokenize(v, s, mode=mm.MMMode.Forward)
+        print("F", results)
 
+        results = mm.tokenize(v, s, mode=mm.MMMode.Reverse)
+        print("R", results)
 
- elif unit == "hmm-short":
-  model = hmm.HMM()
-  model.train(*utils.read_hmm_tagged_sentences("training.txt"))
-  print(model.predict([utils.string_to_ints("迈向充满希望的新世纪——一九九八年新年讲话（附图片１张）")]))
-  # print(list(reversed(model.predict([utils.string_to_ints(reversed("武汉市长江大桥"))]))))
+        results = mm.tokenize(v, s, mode=mm.MMMode.Bidirectional)
+        print("Bi", results)
 
 
- elif unit == "hmm-train":
-  model = hmm.HMM()
-  model.train(*utils.read_hmm_tagged_sentences("training.txt"))
-  _, observations = utils.read_hmm_tagged_sentences("training.txt")
-  predicted_states = model.predict(observations)
-  utils.export_hmm_tagged_sentences(predicted_states, observations, "hmm_training_result.txt")
+    elif unit == "hmm-dummy":
+        states = [[0, 2], [0, 2], [0, 1, 1, 2], [3]]
+        observations = [[53966, 23], [5966, 43], [43, 42, 41, 40], [99]]
+
+        model = hmm.HMM()
+        model.train(states, observations)
+        print(model.predict(observations))
+        print(model.predict([[53966, 23, 5966, 43, 42, 41, 40]]))
 
 
- elif unit == "hmm-export":
-  model = hmm.HMM()
-  model.train(*utils.read_hmm_tagged_sentences("training.txt"))
-  _, observations = utils.read_hmm_tagged_sentences("test.txt")
-  predicted_states = model.predict(observations)
-  utils.export_hmm_tagged_sentences(predicted_states, observations, "hmm_result.txt")
+    elif unit == "hmm-short":
+        model = hmm.HMM()
+        model.train(*utils.read_hmm_tagged_sentences("training.txt"))
+        print(model.predict([utils.string_to_ints("迈向充满希望的新世纪——一九九八年新年讲话（附图片１张）")]))
+        # print(list(reversed(model.predict([utils.string_to_ints(reversed("武汉市长江大桥"))]))))
+
+
+    elif unit == "hmm-train":
+        model = hmm.HMM()
+        model.train(*utils.read_hmm_tagged_sentences("training.txt"))
+        _, observations = utils.read_hmm_tagged_sentences("training.txt")
+        predicted_states = model.predict(observations)
+        utils.export_hmm_tagged_sentences(predicted_states, observations, "hmm_training_result.txt")
+
+
+    elif unit == "hmm-export":
+        model = hmm.HMM()
+        model.train(*utils.read_hmm_tagged_sentences("training.txt"))
+        _, observations = utils.read_hmm_tagged_sentences("test.txt")
+        predicted_states = model.predict(observations)
+        utils.export_hmm_tagged_sentences(predicted_states, observations, "hmm_result.txt")
+
+
+    elif unit == "bigram-test":
+        test_sen1 = "在这个激动人心的时刻，我很高兴通过中国国际广播电台、中央人民广播电台和中央电视台，向全国各族人民，向香港特别行政区同胞、澳门特别行政区同胞和台湾同胞、海外侨胞，向世界各国的朋友们，致以新世纪第一个新年的祝贺！"
+        test_sen2 = "女士们，先生们，同志们，朋友们："
+
+        uni_freq, bi_freq = utils.read_bigram_words("training.txt")
+        bi_gram = bgram.BiGram(uni_freq, bi_freq)
+        print(bi_gram.segment_sentences([test_sen1, test_sen2]))
+
+
+    elif unit == "bigram-export":
+        s = utils.read_plain_sentences("test.txt")
+
+        uni_freq, bi_freq = utils.read_bigram_words("training.txt")
+        bi_gram = bgram.BiGram(uni_freq, bi_freq)
+        results = bi_gram.segment_sentences(s)
+        utils.export_plain_sentences(results, "bgram_result.txt")
